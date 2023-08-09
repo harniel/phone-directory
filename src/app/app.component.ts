d13208126d84fc10f715e2f1f000d2d3d7786022
Harniel Salmeron <harniel@reelmen.com>
2023-08-09 21:34:23 +0800

    added initial implementation for phone-directory

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+interface Contact {
+  name: string;
+  mobile: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'phone-directory';
+  title = 'Phone Directory';
+
+  newContact: Contact = {
+    name: '',
+    mobile: '',
+    email: '',
+  };
+
+  contacts: Contact[] = [];
+
+  private validateForm(): boolean {
+    const namePattern = /^[A-Za-z ]{1,20}$/;
+    const mobilePattern = /^[0-9]{10}$/;
+    const emailPattern =
+      /^[a-zA-Z][a-zA-Z0-9.]{1,10}@[a-zA-Z]{2,20}\.[a-zA-Z]{2,10}$/;
+
+    return (
+      namePattern.test(this.newContact.name) &&
+      mobilePattern.test(this.newContact.mobile) &&
+      emailPattern.test(this.newContact.email)
+    );
+  }
+
+  addCustomer() {
+    if (!this.validateForm()) {
+      document.getElementById('error')!.style.display = 'block';
+      return;
+    }
+
+    document.getElementById('error')!.style.display = 'none';
+    this.contacts.push(this.newContact);
+    this.newContact = {
+      name: '',
+      mobile: '',
+      email: '',
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
